cb97813c2fe798f5d383ff657914d178b3fd9e19
Michael <d1snin@aol.com>
2021-03-08 16:02:42 +0300

    ok

--- src/main/java/xyz/d1snin/emily/commands/AnimeCommand.java
+++ src/main/java/xyz/d1snin/emily/commands/AnimeCommand.java
@@ -1,35 +1,39 @@
 package xyz.d1snin.emily.commands;
-
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import xyz.d1snin.emily.Emily;
-
 import sg4e.danbooru.Danbooru;
 import sg4e.danbooru.DanbooruBuilder;
 import sg4e.danbooru.Post;
-
 import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-
 public class AnimeCommand extends Command {
     @Override
     public void onCommand(MessageReceivedEvent e, String[] args) {
         if (e.getTextChannel().isNSFW()) {
-            String danbooruTags = getArgAsString(args, false);
-            e.getTextChannel().sendMessage(new EmbedBuilder()
-                    .setDescription(getArgAsString(args, true))
-                    .setColor(Color.ORANGE)
-                    .setFooter(Emily.BOT_NAME, e.getJDA().getSelfUser().getAvatarUrl())
-                    .setImage(generateImage(danbooruTags))
-                    .build()).queue();
-        } else {
+            try {
+                String danbooruTags = getArgAsString(args, false);
                 e.getTextChannel().sendMessage(new EmbedBuilder()
-                        .setDescription("Please use this command in NSFW channel!")
+                        .setDescription(getArgAsString(args, true))
+                        .setColor(Color.ORANGE)
+                        .setFooter(Emily.BOT_NAME, e.getJDA().getSelfUser().getAvatarUrl())
+                        .setImage(generateImage(danbooruTags))
+                        .build()).queue();
+            } catch (IllegalArgumentException illegalArgumentException) {
+                e.getTextChannel().sendMessage(new EmbedBuilder()
+                        .setDescription("Could not find an image for this tag.")
                         .setColor(Color.ORANGE)
                         .setFooter(Emily.BOT_NAME, e.getJDA().getSelfUser().getAvatarUrl())
                         .build()).queue();
+            }
+        } else {
+            e.getTextChannel().sendMessage(new EmbedBuilder()
+                    .setDescription("Please use this command in NSFW channel!")
+                    .setColor(Color.ORANGE)
+                    .setFooter(Emily.BOT_NAME, e.getJDA().getSelfUser().getAvatarUrl())
+                    .build()).queue();
         }
     }
 
@@ -40,7 +44,7 @@ public class AnimeCommand extends Command {
 
     @Override
     public String getDescription() {
-        return "Random anime picture from Danbooru (NSFW). You can search by tag - `'anime <tag>`";
+        return "Random anime picture from Danbooru (NSFW).щщзYou can search by tag - `'anime <tag>`";
     }
     private static String getArgAsString(String[] args, boolean quotes) {
         if (quotes) {
