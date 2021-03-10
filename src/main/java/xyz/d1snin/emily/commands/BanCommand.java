575c978ea3808d061707665b51ae886195a886c9
Michael <d1snin@aol.com>
2021-03-10 08:03:47 +0300

    ban command update

--- src/main/java/xyz/d1snin/emily/commands/BanCommand.java
+++ src/main/java/xyz/d1snin/emily/commands/BanCommand.java
@@ -3,6 +3,7 @@ package xyz.d1snin.emily.commands;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.PrivateChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import xyz.d1snin.emily.Emily;
 
@@ -11,11 +12,12 @@ import java.util.Arrays;
 import java.util.List;
 
 public class BanCommand extends Command {
+    private static String reason = "";
     @Override
     public void onCommand(MessageReceivedEvent e, String[] args) {
         if (args.length < 2) {
             e.getTextChannel().sendMessage(new EmbedBuilder()
-                    .setDescription("Please use the following syntax: " + "`" + Emily.BOT_PREFIX + "ban <mentionTheUser> <Reason>`")
+                    .setDescription("Please use the following syntax: " + "`" + Emily.BOT_PREFIX + "ban <mentionTheUser> <delete msgs: yes or no> <Reason>`")
                     .setFooter(Emily.BOT_NAME, e.getJDA().getSelfUser().getAvatarUrl())
                     .setColor(Color.ORANGE)
                     .build()).queue();
@@ -28,19 +30,28 @@ public class BanCommand extends Command {
                     .setColor(Color.ORANGE)
                     .build()).queue();
         } else {
-            String reason = "";
-            for (int i = 2; i < args.length; i++) {
+            int deleteMsgs = (args[2].equals("yes") ? 24 : 0);
+            for (int i = 3; i < args.length; i++) {
                 reason += args[i];
             }
             List<Member> mentionedMembers = e.getMessage().getMentionedMembers();
             Member target = mentionedMembers.get(0);
-            target.ban(7).reason(reason).queue();
+            target.ban(deleteMsgs).reason(reason).queue();
                 e.getTextChannel().sendMessage(new EmbedBuilder()
                         .setDescription("User " + target.getAsMention() + " has been banned by " + e.getAuthor().getAsMention() + "\nReason: " + reason)
                         .setFooter(Emily.BOT_NAME, e.getJDA().getSelfUser().getAvatarUrl())
                         .setColor(Color.ORANGE)
                         .build()).queue();
+                sendPrivate(e.getAuthor().openPrivateChannel().complete(), args, e);
         }
+    }
+    private void sendPrivate(PrivateChannel channel, String[] args, MessageReceivedEvent e)
+    {
+        channel.sendMessage(new EmbedBuilder()
+                .setDescription("You have been banned from the server by " + e.getAuthor().getAsMention() + "\nReason: " + reason)
+                .setColor(Color.ORANGE)
+                .setFooter(Emily.BOT_NAME, Emily.getAPI().getSelfUser().getAvatarUrl())
+                .build()).queue();
     }
     @Override
     public List<String> getAliases()
