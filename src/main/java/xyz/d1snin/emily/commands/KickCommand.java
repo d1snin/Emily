4346104a80fac9eba72894b6768b06b1022d95e2
Michael <d1snin@aol.com>
2021-03-10 08:14:28 +0300

    update ban command and kick command added

--- src/main/java/xyz/d1snin/emily/commands/KickCommand.java
+++ src/main/java/xyz/d1snin/emily/commands/KickCommand.java
@@ -11,52 +11,51 @@ import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 
-public class BanCommand extends Command {
+public class KickCommand extends Command {
     private static String reason = "";
     @Override
     public void onCommand(MessageReceivedEvent e, String[] args) {
         if (args.length < 2) {
             e.getTextChannel().sendMessage(new EmbedBuilder()
-                    .setDescription("Please use the following syntax: " + "`" + Emily.BOT_PREFIX + "ban <mentionTheUser> <delete msgs: yes or no> <Reason>`")
+                    .setDescription("Please use the following syntax: " + "`" + Emily.BOT_PREFIX + "kick <mentionTheUser> <Reason>`")
                     .setFooter(Emily.BOT_NAME, e.getJDA().getSelfUser().getAvatarUrl())
                     .setColor(Color.ORANGE)
                     .build()).queue();
             return;
         }
-        if (!e.getMember().hasPermission(Permission.BAN_MEMBERS)) {
+        if (!e.getMember().hasPermission(Permission.KICK_MEMBERS)) {
             e.getTextChannel().sendMessage(new EmbedBuilder()
                     .setDescription("You dont have permission to use this command.")
                     .setFooter(Emily.BOT_NAME, e.getJDA().getSelfUser().getAvatarUrl())
                     .setColor(Color.ORANGE)
                     .build()).queue();
         } else {
-            int deleteMsgs = (args[2].equals("yes") ? 24 : 0);
-            for (int i = 3; i < args.length; i++) {
+            for (int i = 2; i < args.length; i++) {
                 reason += args[i];
             }
             List<Member> mentionedMembers = e.getMessage().getMentionedMembers();
             Member target = mentionedMembers.get(0);
-            target.ban(deleteMsgs).reason(reason).queue();
+            target.kick(reason).queue();
+            e.getTextChannel().sendMessage(new EmbedBuilder()
+                    .setDescription("User " + target.getAsMention() + " has been kicked by " + e.getAuthor().getAsMention() + "\nReason: " + reason)
+                    .setFooter(Emily.BOT_NAME, e.getJDA().getSelfUser().getAvatarUrl())
+                    .setColor(Color.ORANGE)
+                    .build()).queue();
+            try {
+                sendPrivate(e.getAuthor().openPrivateChannel().complete(), args, e);
+            } catch (RuntimeException exception) {
                 e.getTextChannel().sendMessage(new EmbedBuilder()
-                        .setDescription("User " + target.getAsMention() + " has been banned by " + e.getAuthor().getAsMention() + "\nReason: " + reason)
+                        .setDescription("It is impossible to write a message to the user, perhaps the DM is closed")
                         .setFooter(Emily.BOT_NAME, e.getJDA().getSelfUser().getAvatarUrl())
                         .setColor(Color.ORANGE)
                         .build()).queue();
-                try {
-                    sendPrivate(e.getAuthor().openPrivateChannel().complete(), args, e);
-                } catch (RuntimeException exception) {
-                    e.getTextChannel().sendMessage(new EmbedBuilder()
-                            .setDescription("It is impossible to write a message to the user, perhaps the DM is closed")
-                            .setFooter(Emily.BOT_NAME, e.getJDA().getSelfUser().getAvatarUrl())
-                            .setColor(Color.ORANGE)
-                            .build()).queue();
-                }
+            }
         }
     }
     private void sendPrivate(PrivateChannel channel, String[] args, MessageReceivedEvent e)
     {
         channel.sendMessage(new EmbedBuilder()
-                .setDescription("You have been banned from the server" + e.getGuild().getName() + " by " + e.getAuthor().getAsMention() + "\nReason: " + reason)
+                .setDescription("You have been kicked from the server " + e.getGuild().getName() + " by " + e.getAuthor().getAsMention() + "\nReason: " + reason)
                 .setColor(Color.ORANGE)
                 .setFooter(Emily.BOT_NAME, Emily.getAPI().getSelfUser().getAvatarUrl())
                 .build()).queue();
@@ -64,12 +63,12 @@ public class BanCommand extends Command {
     @Override
     public List<String> getAliases()
     {
-        return Arrays.asList(Emily.BOT_PREFIX + "ban");
+        return Arrays.asList(Emily.BOT_PREFIX + "kick");
     }
 
     @Override
     public String getDescription()
     {
-        return "Ban a member";
+        return "Kick a member";
     }
 }
